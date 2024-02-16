4820748241f92658b8d8917e094dd475c111652c
Carlos Pio <carlospiobenicio@gmail.com>
2024-02-16 16:52:54 -0300

    React Helmet (SEO)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { RouterProvider } from 'react-router-dom'
 import './global.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
 import { router } from './routes'
 
 export function App() {
   return (
     <>
-       <RouterProvider router={router} />
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | Pizza.shop" />
+        <RouterProvider router={router} />
+      </HelmetProvider>
     </>
   )
 }
